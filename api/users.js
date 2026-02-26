530bd3a962eb1befd1bd98befd23325a323fb5c1
David Arrazola <arrazola.dvd@gmail.com>
2026-02-26 18:59:33 -0500

    ADD routing middleware that takes care user login. Username and pass are sent to backend. Password is compared with bycrpt. If successfull, token is created and returned to user

--- api/users.js
+++ api/users.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import { createUser } from "#db/queries/users";
+import { createUser, getUserByEmailAndPass } from "#db/queries/users";
 import { createToken } from "#utils/jwt";
 
 const userRouter = express.Router();
 export default userRouter;
+
+const NUM_SALT = 5;
 
 userRouter.post("/register", async (req, res) => {
   try {
@@ -17,13 +19,30 @@ userRouter.post("/register", async (req, res) => {
       return res.status(400).send("Username or password is missing");
 
     //! Bcrypt password salt's the hash 5 times
-    const hashPassword = await bcrypt.hash(password, 5);
+    const hashPassword = await bcrypt.hash(password, NUM_SALT);
     //* Creating a new user with the username and hashed password
     const newUser = await createUser(username, hashPassword);
     //! Sending ID of newly created user as the PAYLOAD for JWT creation
     const token = await createToken({ id: newUser.id });
 
     res.status(201).send(token);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    if (!req.body) return res.status(400).send("Request body is missing");
+    const { username, password } = req.body;
+
+    if (!username || !password)
+      return res.status(400).send("Body is missing fields");
+
+    const selectedUser = await getUserByEmailAndPass(username, password);
+    const token = await createToken({ id: selectedUser.id });
+
+    res.status(200).send(token);
   } catch (e) {
     console.error(e);
   }
--- db/queries/users.js
+++ db/queries/users.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import bcrypt from "bcrypt";
 
 export async function createUser(username, password) {
   try {
@@ -8,8 +9,25 @@ export async function createUser(username, password) {
             RETURNING *;
         `;
     const newUser = (await db.query(sql, [username, password])).rows;
-
     return newUser[0];
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function getUserByEmailAndPass(username, password) {
+  try {
+    const sql = `SELECT * FROM users WHERE username = $1`;
+    //! Retrieving the user info based on username provided in request.
+    const selectedUser = (await db.query(sql, [username])).rows;
+
+    //! Using "bcyrpt.compare()" to see if the password from request and hashed
+    //! password saved in the retrieved user match
+    const isValid = await bcrypt.compare(password, selectedUser[0].password);
+
+    if (!isValid) return null;
+
+    return selectedUser[0];
   } catch (e) {
     console.error(e);
   }
